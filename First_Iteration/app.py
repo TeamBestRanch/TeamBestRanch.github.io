f703578f2cbec403f4b9bac2232019b70f89c039
Tiffanny <tiffannyhernaez@gmail.com>
2020-09-24 14:58:26 -0700

    Refined the frontend with Diana's code

--- First_Iteration/app.py
+++ First_Iteration/app.py
@@ -10,7 +10,7 @@ app = Flask(__name__,template_folder='templates')
 
 
 @app.route("/")
-@app.route("/home")
+@app.route("/home",  methods=['GET', 'POST'])
 def Home():
     return render_template("index.html")
 
