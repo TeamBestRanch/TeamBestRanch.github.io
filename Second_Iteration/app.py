76539f527bff8584f6eb30e52dfb784e3fa5841b
Diana-Joya <d_joya@csu.fullerton.edu>
2020-10-06 02:56:54 -0700

    Worked on adding Restaurant and Rating Button tables to db

--- Second_Iteration/app.py
+++ Second_Iteration/app.py
@@ -15,12 +15,35 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     img_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
+    scores = db.relationship('RatingButton', backref='author', lazy=True)
 
     def __repr__(self):
         return f"User('{self.fname}','{self.lname}', '{self.email}', '{self.img_file}')"
 
     # [insert variable name] = db.relationship('[insert class name], backref='', lazy = True) --> To be used to grab info from other tables??
     # db.ForeignKey --> to be used later
+
+
+class Restaurant(db.Model):
+    restaurant_id = db.Column(db.Integer, primary_key=True)
+    restaurant_name = db.Column(db.String(100), nullable=False)
+    ranch_name = db.Column(db.String(100), nullable=False)
+    ranch_img = db.Column(db.String(20), nullable=False, default='default.jpg')
+    avg_score = db.Column(db.Integer, nullable=False)
+    scores = db.relationship('RatingButton', backref='rated', lazy=True)
+
+    def __repr__(self):
+        return f"Restaurant('{self.restaurant_name}', '{self.ranch_name}', '{self.ranch_img}', '{self.avg_score}')"
+
+
+class RatingButton(db.Model):
+    rate_id = db.Column(db.Integer, primary_key=True)
+    customer_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.restaurant_id'), nullable=False)
+    score = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Rating('{self.score}')"
 
 
 @app.route("/")
