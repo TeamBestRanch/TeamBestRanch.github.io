bf6297567c4537a07ffff6e229819ae8a7511ed5
Tiffanny <tiffannyhernaez@gmail.com>
2020-10-11 03:41:03 -0700

    User local DB created (SQLite), still need to fix verification/security for user

--- Second_Iteration/app.py
+++ Second_Iteration/app.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__, template_folder='templates')
+# Database configurations
 app.config['SECRET_KEY'] = 'faef9429c2bad94dfadbf9f24276e00f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+
+# User Authentication
+bcrypt = Bcrypt(app)
+
+# Database for User, RatingButton, and Restaurant
 
 
 class User(db.Model):
@@ -15,14 +22,38 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     img_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
+    scores = db.relationship('RatingButton', backref='author', lazy=True)
 
     def __repr__(self):
         return f"User('{self.fname}','{self.lname}', '{self.email}', '{self.img_file}')"
 
-    # [insert variable name] = db.relationship('[insert class name], backref='', lazy = True) --> To be used to grab info from other tables??
-    # db.ForeignKey --> to be used later
+
+class Restaurant(db.Model):
+    restaurant_id = db.Column(db.Integer, primary_key=True)
+    restaurant_name = db.Column(db.String(100), nullable=False)
+    ranch_name = db.Column(db.String(100), nullable=False)
+    ranch_img = db.Column(
+        db.String(20), nullable=False, default='default.jpg')
+    avg_score = db.Column(db.Integer, nullable=False)
+    scores = db.relationship('RatingButton', backref='rated', lazy=True)
+
+    def __repr__(self):
+        return f"Restaurant('{self.restaurant_name}', '{self.ranch_name}', '{self.ranch_img}', '{self.avg_score}')"
 
 
+class RatingButton(db.Model):
+    rate_id = db.Column(db.Integer, primary_key=True)
+    customer_id = db.Column(db.Integer, db.ForeignKey(
+        'user.user_id'), nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey(
+        'restaurant.restaurant_id'), nullable=False)
+    score = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Rating('{self.score}')"
+
+
+# Page Routes
 @app.route("/")
 @app.route("/home",  methods=['GET', 'POST'])
 def Home():
@@ -33,7 +64,7 @@ def Home():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@ranch.com' and form.password.data == 'password':
+        if form.email.data == 'admin@LIS.com' and form.password.data == 'ranch':
             flash('You have been logged in!', 'success')
             return redirect(url_for('login'))
         else:
@@ -45,9 +76,18 @@ def login():
 def signup():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(
-            f'Account Created for {form.fname.data} {form.lname.data}!', 'success')
-        return redirect(url_for('signup'))
+        # Encrypts User info
+        hashed_password = bcrypt.generate_password_hash(
+            form.password.data).decode('utf-8')
+        user = User(fname=form.fname.data, lname=form.lname.data,
+                    email=form.email.data, password=hashed_password)
+
+        # Adds the new user into the database
+        db.session.add(user)
+        db.session.commit()
+
+        flash('Your Account has been created! You can now log in', 'success')
+        return redirect(url_for('login'))
     return render_template('signup.html', title='Sign Up', form=form)
 
 
--- Second_Iteration/forms.py
+++ Second_Iteration/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
-
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+# from app import User NEED TO FIX  CAN'T IMPORT FROM APP RIGHT NOW
 
 class RegistrationForm(FlaskForm):
     fname = StringField('First Name', validators=[
@@ -14,6 +14,11 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField(
         'Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit_signup = SubmitField('Sign Up')
+    
+    def validate_field(self, email):
+        user = User.query.filter_by(email = email.data).all()
+        if user:
+            raise ValidationError('Account for email already exists!')
 
 
 class LoginForm(FlaskForm):
